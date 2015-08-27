539edb888de6d32eacaec5d04ba02aa709f44710
Ramsay Lanier <ramsay@nclud.com>
2015-08-27 15:15:47 -0400

    init

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'ramsay:react-components-nav',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'React Nav component',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use([
+    'ramsay:react-components-dependencies@0.0.1'
+  ], 'client');
+
+
+  api.addFiles('component-nav.jsx', 'client');
+
+  api.export(['NavList', 'SubNavList', 'NavItem', 'Navs'], 'client');
+});
